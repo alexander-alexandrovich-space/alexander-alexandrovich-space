de7fc252fb56be644764a9962dcf44e6923ade7e
Alexander Alexandrovich <Sasha89600152611@gmail.com>
2026-03-09 13:02:11 +0300

    a

--- simulation of intersatellite network/LoRa/LoRa_tx.m
+++ simulation of intersatellite network/LoRa/LoRa_tx.m
@@ -28,39 +28,39 @@ preamble = repmat(base_chirp,1,cfg.Preamble);
 tx = [preamble tx];
 
 %%
-% Fc = 868e6;
-% t = (0:length(tx)-1)/Fs;
-% rf = real(tx .* exp(1j*2*pi*Fc*t));
-% 
-% figure;
-% plot(t(1:10000), rf(1:10000));
-% grid on;
-% xlabel('Time (s)');
-% ylabel('Amplitude');
-% title('LoRa RF Signal (Time Domain)');
+Fc = 868e6;
+t = (0:length(tx)-1)/Fs;
+rf = real(tx .* exp(1j*2*pi*Fc*t));
+
+figure;
+plot(t(1:10000), rf(1:10000));
+grid on;
+xlabel('Time (s)');
+ylabel('Amplitude');
+title('LoRa RF Signal (Time Domain)');
+%
+N = length(rf);
+f = (-N/2:N/2-1)*(Fs/N);
+
+RF_spectrum = fftshift(abs(fft(rf)));
+
+figure;
+plot(Fc + f, 20*log10(RF_spectrum));
+grid on;
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+title('LoRa Spectrum around Carrier');
+
+
 %%
-% N = length(rf);
-% f = (-N/2:N/2-1)*(Fs/N);
-% 
-% RF_spectrum = fftshift(abs(fft(rf)));
-% 
-% figure;
-% plot(Fc + f, 20*log10(RF_spectrum));
-% grid on;
-% xlabel('Frequency (Hz)');
-% ylabel('Magnitude (dB)');
-% title('LoRa Spectrum around Carrier');
-% 
-% 
-% %%
-% 
-% window = 1024;
-% noverlap = 900;
-% nfft = 2048;
-% 
-% figure;
-% spectrogram(tx, window, noverlap, nfft, cfg.BW, 'yaxis');
-% colormap jet;
-% colorbar;
-% title('LoRa Baseband Chirp');
+
+window = 1024;
+noverlap = 900;
+nfft = 2048;
+
+figure;
+spectrogram(tx, window, noverlap, nfft, cfg.BW, 'yaxis');
+colormap jet;
+colorbar;
+title('LoRa Baseband Chirp');
 end
